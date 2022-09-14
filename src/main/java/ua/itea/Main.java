17a6727018f7d3e21cc8cbecc0c4c26aef4e1bca
PavloMak <pavlo9714@gmail.com>
2022-09-14 17:11:44 +0300

    final commit

--- src/main/java/ua/itea/Main.java
+++ src/main/java/ua/itea/Main.java
@@ -16,45 +16,50 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter the path to xml files.");
 		String path = scanner.nextLine();
-		Manager filemager = new Manager(path);
+		File dir = new File(path);
+		if (dir.isDirectory()) {
+			Manager filemager = new Manager(dir);
 
-		System.out.println("Do you wish to clear database before usage? (Y/N)");
+			System.out.println("Do you wish to clear database before usage? (Y/N)");
 
-		String clearing;
-		if (scanner.hasNext("Y") || scanner.hasNext("N")) {
-			clearing = scanner.next();
+			String clearing;
+			if (scanner.hasNext("Y") || scanner.hasNext("N")) {
+				clearing = scanner.next();
 
-			if (clearing.equals("Y")) {
-				manager.createBD(true);
+				if (clearing.equals("Y")) {
+					manager.createBD(true);
+				} else {
+					manager.createBD(false);
+				}
+
 			} else {
-				manager.createBD(false);
+				System.out.println("Wrong input");
+				scanner.next();
 			}
 
-		} else {
-			System.out.println("Wrong input");
-			scanner.next();
-		}
-
-		CountDownLatch waiter = new CountDownLatch(2);
-		FileParser parcer1 = new FileParser("A", filemager, manager, waiter);
-		FileParser parcer2 = new FileParser("B", filemager, manager, waiter);
-		try {
-			waiter.await();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Do you wish to see results (Y/N)");
-		String answer;
-		if (scanner.hasNext("Y") || scanner.hasNext("N")) {
-			answer = scanner.next();
-
-			if (answer.equals("Y")) {
-				manager.getAllInfo();
+			CountDownLatch waiter = new CountDownLatch(2);
+			FileParser parcer1 = new FileParser("A", filemager, manager, waiter);
+			FileParser parcer2 = new FileParser("B", filemager, manager, waiter);
+			try {
+				waiter.await();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
+			System.out.println("Do you wish to see results (Y/N)");
+			String answer;
+			if (scanner.hasNext("Y") || scanner.hasNext("N")) {
+				answer = scanner.next();
 
+				if (answer.equals("Y")) {
+					manager.getAllInfo();
+				}
+
+			} else {
+				System.out.println("Wrong input");
+				scanner.next();
+			}
 		} else {
-			System.out.println("Wrong input");
-			scanner.next();
+			System.out.println("Wrong input!");
 		}
 
 	}
--- src/main/java/ua/itea/Manager.java
+++ src/main/java/ua/itea/Manager.java
@@ -11,25 +11,48 @@ public class Manager {
 
 	private String currentDir;
 	private List<File> files;
+	private boolean nofiles = true;
 
-	public Manager(String path) {
-		File dir = new File(path);
+	public Manager(File dir) {
+
 		files = new ArrayList<File>();
 		File[] content = dir.listFiles();
-		for (int i = 0; i < content.length; i++) {
-			if (content[i].isFile()) {
-				files.add(content[i]);
+		if (!content.equals(null)) {
+			for (int i = 0; i < content.length; i++) {
+				if (content[i].isFile()) {
+					files.add(content[i]);
+				}
 			}
+			nofiles = false;
+			cleanList();
 		}
+
 	}
 
 	public synchronized File getFile() {
 		File toreturn = null;
-		if (files.size() != 0) {
-			toreturn = files.get(0);
-			files.remove(0);
+		if (!nofiles) {
+			if (files.size() != 0) {
+				toreturn = files.get(0);
+				files.remove(0);
+			}
 		}
+
 		return toreturn;
 	};
+
+	private void cleanList() {
+		File temp = null;
+		int i = 0;
+		while (i < files.size()) {
+			temp = files.get(i);
+			if (temp.getName().endsWith(".xml")) {
+				i++;
+			} else {
+				files.remove(i);
+			}
+		}
+		System.out.println(files.size());
+	}
 
 }
